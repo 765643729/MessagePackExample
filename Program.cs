d04313988c404a71f31897ef1f9c2927b0557609
JZH <765643729@163.com>
2021-09-22 20:01:04 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using MessagePack;
+using System;
+
+namespace MessagePackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entity = new TestEntity()
+            {
+                BoolTest = true,
+                DoubleTest = 123.003,
+                FloatTest = 564.36f,
+                IntArrayTest = new int[3] { 3, 2, 4 },
+                IntTest = 99,
+                StringTest = "测试StringTest",
+                IgnoreMemberTest = "测试IgnoreMemberTest",
+            };
+
+            var json = MessagePackSerializer.SerializeToJson(entity);
+            var data = MessagePackSerializer.Serialize(entity);
+            Console.WriteLine(json);
+            Console.WriteLine(data);
+
+            TestEntity entityFromBlob = MessagePackSerializer.Deserialize<TestEntity>(data);
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ TestEntity.cs
@@ -0,0 +1,34 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagePackExample
+{
+    [MessagePackObject]
+    public class TestEntity
+    {
+        [Key(0)]
+        public int IntTest { get; set; }
+
+        [Key(1)]
+        public string StringTest { get; set; }
+
+        [Key(2)]
+        public float FloatTest { get; set; }
+
+        [Key(3)]
+        public double DoubleTest { get; set; }
+
+        [Key(4)]
+        public bool BoolTest { get; set; }
+
+        [Key(5)]
+        public int[] IntArrayTest { get; set; }
+
+        [IgnoreMember]
+        public string IgnoreMemberTest { get; set; }
+    }
+}
